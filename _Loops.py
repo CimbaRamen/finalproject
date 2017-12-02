bcbd7b58a6531ed7f96aa63710169a60e4f9f02f
CimbaRamen <cimfenglee@gmail.com>
2017-12-02 11:54:32 -0600

    Practice Commit to master

---
+++ _Loops.py
@@ -0,0 +1,23 @@
+'''
+Created on Dec 2, 2017
+
+@author: ITAUser
+'''
+
+numberlist=[];
+numberlist.append(3);
+
+print (numberlist);
+
+numberlist.append(5);
+numberlist.append(7);
+
+print (numberlist);
+
+
+for i in range(5):
+    if (i%10 = 0):
+        numberlist.append(i);
+print(numberlist)
+
+hi
