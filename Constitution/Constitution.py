0cc31268e0269fc871c14a43cd1a274378936d69
CimbaRamen <cimfenglee@gmail.com>
2018-02-17 11:51:13 -0600

    good stuff

--- Constitution/Constitution.py
+++ Constitution/Constitution.py
@@ -1,14 +1,18 @@
 
+
 filename = "constitution.txt"
 numberfile = open("constitution.txt","r+");
 
-numberwords = 0
+numberwords=0
+numberchar=0
 
 for line in numberfile:
     words=line.split();
     numberwords = numberwords+ len(words);
-    
-print (numberwords);
+    for word in words:
+        numberchar= numberchar + word.count ("p")
+
+print (numberchar);
 
 
 
